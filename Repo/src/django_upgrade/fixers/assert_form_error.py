19dc48c3c98d8c40c8361c434b0c7aa8891844fa
Gourav Sen <gourav.s@turing.com>
2025-08-22 00:13:55 +0530

    Turn 2: Model A response

--- Repo/src/django_upgrade/fixers/assert_form_error.py
+++ Repo/src/django_upgrade/fixers/assert_form_error.py
@@ -10,17 +10,15 @@ import ast
 from functools import partial
 from typing import Iterable
 
-from tokenize_rt import UNIMPORTANT_WS, Offset, Token, tokens_to_src
+from tokenize_rt import Offset, Token
 
 from django_upgrade.ast import ast_start_offset
 from django_upgrade.data import Fixer, State, TokenFunc
 from django_upgrade.tokens import (
     OP,
-    PHYSICAL_NEWLINE,
-    consume,
-    find_final_token,
+    UNIMPORTANT_WS,
+    find,
     find_first_token,
-    reverse_consume,
 )
 
 fixer = Fixer(
@@ -42,19 +40,17 @@ def visit_Call(
         and node.func.value.id == "self"
         and len(node.args) in (4, 5)
         and len(node.keywords) == 0
-        and isinstance((first_arg := node.args[0]), ast.Name)
-        and (first_arg.id == "response" or "response" in first_arg.id and len(first_arg.id) > 8)
+        and isinstance(node.args[0], ast.Name)
         and (
-            (
-                isinstance((second_arg := node.args[1]), ast.Constant)
-                and isinstance(second_arg.value, (str, int))
-            )
+            isinstance(node.args[1], ast.Constant)
+            and isinstance(node.args[1].value, str)
+            or isinstance(node.args[1], ast.Name)
         )
     ):
-        yield ast_start_offset(first_arg), partial(
+        yield ast_start_offset(node.args[0]), partial(
             rewrite_args,
-            response_arg=first_arg,
-            form_arg=second_arg,
+            response_arg=node.args[0],
+            form_arg=node.args[1],
         )
 
 
@@ -65,15 +61,29 @@ def rewrite_args(
     response_arg: ast.Name,
     form_arg: ast.Constant | ast.Name,
 ) -> None:
-    j = find_first_token(tokens, i, node=form_arg)
-    k = find_final_token(tokens, j, node=form_arg)
-    ftokens = tokens[j:k]
-    k = consume(tokens, k, name=OP, src=",")
-    if k < len(tokens) - 2 and tokens[k].name == PHYSICAL_NEWLINE:
-        j = reverse_consume(tokens, j, name=UNIMPORTANT_WS)
-    del tokens[j : k]  # removed +1, will leave extra comma
+    # Find the response token
+    response_idx = find_first_token(tokens, i, node=response_arg)
 
-    rtoken = tokens[i]
-    tokens[i] = rtoken._replace(
-        src=rtoken.src + " .context[" + tokens_to_src(ftokens) + "]",
+    # Find the form token
+    form_idx = find_first_token(tokens, response_idx + 1, node=form_arg)
+
+    # Get the form value
+    form_str = tokens[form_idx].src
+
+    # Replace the response token with response.context[form]
+    tokens[response_idx] = tokens[response_idx]._replace(
+        src=tokens[response_idx].src + ".context[" + form_str + "]"
     )
+
+    # Find the comma after response
+    comma_idx = response_idx + 1
+    while comma_idx < len(tokens) and tokens[comma_idx].src != ",":
+        comma_idx += 1
+
+    # Find the token after form (should be a comma)
+    after_form_idx = form_idx + 1
+    while after_form_idx < len(tokens) and tokens[after_form_idx].name == UNIMPORTANT_WS:
+        after_form_idx += 1
+
+    # Delete from the comma after response up to (but not including) the comma after form
+    del tokens[comma_idx:after_form_idx]
